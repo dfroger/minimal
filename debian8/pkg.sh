945edb5332f938c2a0b98e7c2ce1919f881e51df
David Froger <david.froger@mailoo.org>
2016-07-21 17:35:59 +0200

    Add multitail

--- debian8/pkg.sh
+++ debian8/pkg.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apt-get install --yes --force-yes git screen vim realpath tree curl
+apt-get install --yes --force-yes \
+    curl \
+    git \
+    multitail \
+    realpath \
+    screen \
+    tree \
+    vim
